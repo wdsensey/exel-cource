6bfd7623453339280ab61c570d8f2a49ade8e867
Alexandr Kuzmin <kuzmin1913@gmail.com>
2020-12-07 17:45:57 +0200

    Finish base framework functional (#2)

---
+++ src/components/formula/Formula.js
@@ -0,0 +1,26 @@
+import {ExcelComponent} from '@core/ExcelComponent';
+
+export class Formula extends ExcelComponent {
+    static className = 'excel__formula'
+    constructor($root) {
+      super($root, {
+        name: 'Formula',
+        listeners: ['mousedown', 'mouseup', 'input'],
+      });
+    }
+    toHTML() {
+      return `
+        <div class="info">fx</div>
+        <div class="input" contenteditable spellcheck="false"></div>
+      `
+    }
+    onInput(event) {
+
+    }
+    onMousedown() {
+
+    }
+    onMouseup() {
+
+    }
+}
--- webpack.config.js
+++ webpack.config.js
@@ -14,7 +14,12 @@ const jsLoaders = () => {
     {
       loader: 'babel-loader',
       options: {
-        presets: ['@babel/preset-env'],
+        presets: [
+          '@babel/preset-env',
+        ],
+        plugins: [
+          '@babel/plugin-proposal-class-properties',
+        ],
       },
     },
   ]
